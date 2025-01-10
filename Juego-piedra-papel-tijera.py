076999ae458eab4c206a0771b3dab229230af7ba
Heber <heber_rocha@outlook.com.ar>
2025-01-09 22:55:14 -0300

    Solo comentarios

--- Juego-piedra-papel-tijera.py
+++ Juego-piedra-papel-tijera.py
@@ -3,9 +3,9 @@ turno_max = 4
 final = False
 
 
-while not final and turno < turno_max:
+while not final and turno < turno_max: #IMPORTANTE DETERMINAR LAS VARIABLES QUE DETERMINAN LA VICTORIA DENTRO DEL BUCLE PARA QUE ESTAS VAYAN CAMBIANDO SEGUN LOS RESULTADOS. SINO SE CREA BUCLE INFINITO
 
-    nombre1 = input("¿Cual sera el nombre del jugador 1?: ")
+    nombre1 = input("¿Cual sera el nombre del jugador 1?: ") 
     nombre2 = input("¿Cual sera el nombre del jugador 2?: ")
 
     print("(Max 3 turno) Turno:" , turno)
@@ -20,13 +20,13 @@ while not final and turno < turno_max:
 
     elif (jugador1 == "piedra" and jugador2 == "tijera") or (jugador1 == "papel" and jugador2 == "piedra") or (jugador1 == "tijera" and jugador2 == "papel"):
         print("Ganador: " , nombre1)
-        final = True
+        final = True  #IMPORTANTE DEFINIR EL BOOLEANO = TRUE EN TODAS LAS POSIBILIDADES DE VICTORIA
     
     else:
         print("Ganador: " , nombre2)
-        final = True
+        final = True #IMPORTANTE DEFINIR EL BOOLEANO = TRUE EN TODAS LAS POSIBILIDADES DE VICTORIA
 
-    turno += 1
+    turno += 1   #PONER LA SUMATORIA A LO ULTIMO DEL CODIGO PARA QUE SE VAYA SUMANDO DESPUES DE UN TURNO, IMPORTANTE RESPETAR EL ORDEN DEL CODIGO EN PYTHON
 
     if not turno < turno_max:
         print("Game over")
