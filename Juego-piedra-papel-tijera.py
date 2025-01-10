a86c3dbed5926d95fe7498e422c127fbdfaac555
Heber <heber_rocha@outlook.com.ar>
2025-01-09 22:50:11 -0300

    Agregue algunos cambios trivales

--- Juego-piedra-papel-tijera.py
+++ Juego-piedra-papel-tijera.py
@@ -4,21 +4,26 @@ final = False
 
 
 while not final and turno < turno_max:
+
+    nombre1 = input("¿Cual sera el nombre del jugador 1?: ")
+    nombre2 = input("¿Cual sera el nombre del jugador 2?: ")
+
     print("(Max 3 turno) Turno:" , turno)
 
-    jugador1 = input("Piedra, Papel o tijera: ")
-    jugador2 = input("Piedra, Papel o tijera: ")
+    jugador1 = input("¿Que elije el jugador 1? ¿Piedra, Papel o tijera?: ")
+    jugador2 = input("¿Que elije el jugador ? ¿Piedra, Papel o tijera?: ")
+    
 
 
     if jugador1 == jugador2:
         print("Empate")
 
     elif (jugador1 == "piedra" and jugador2 == "tijera") or (jugador1 == "papel" and jugador2 == "piedra") or (jugador1 == "tijera" and jugador2 == "papel"):
-        print("Gana Jugador 1")
+        print("Ganador: " , nombre1)
         final = True
     
     else:
-        print("Gana jugador 2")
+        print("Ganador: " , nombre2)
         final = True
 
     turno += 1
